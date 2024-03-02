d57e67b79a36840756c63c9b2aefb5f51c592d4d
Bazilansari <154603848+Bazilansari@users.noreply.github.com>
2024-03-02 13:57:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// --------------Sum---------------
+
+var numb1 = +prompt('Sum Amount')
+var numb2 = +prompt('Sum Amount')
+
+var Sum = numb1 + numb2
+console.log(Sum);
+
+
+
+// --------------Sub---------------
+
+var numb1 = +prompt('Sub Amount')
+var numb2 = +prompt('Sub Amount')
+
+var Sub = numb1 - numb2
+console.log(Sub);
+
+
+// --------------Multiply---------------
+
+var numb1 = +prompt('Multiply Amount')
+var numb2 = +prompt('Multiply Amount')
+
+var Multiply = numb1 * numb2
+console.log(Multiply);
+
+
+// --------------Divide---------------
+
+var numb1 = +prompt('Divide Amount')
+var numb2 = +prompt('Divide Amount')
+
+var Divide = numb1 / numb2
+console.log(Divide);
+
+
+// --------------Reminder ---------------
+
+var numb1 = +prompt('Reminder  Amount')
+var numb2 = +prompt('Reminder  Amount')
+
+var Reminder  = numb1 % numb2
+console.log(Reminder );
+
+
+
